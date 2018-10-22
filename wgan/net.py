8787d466b0a581072edf9d92323eda418cb05e8d
TheRevanchist <ismail.elezi@gmail.com>
2018-10-22 22:07:01 +0200

    Add nets for Wasserstein Gan

---
+++ wgan/net.py
@@ -0,0 +1,52 @@
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, latent, channels, num_filters):
+        super(Generator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(latent, num_filters * 8, 4, 1, 0, bias=False),
+            nn.ReLU(True),
+            nn.BatchNorm2d(num_filters * 8),
+            nn.ConvTranspose2d(num_filters * 8, num_filters * 4, 4, 2, 1, bias=False),
+            nn.ReLU(True),
+            nn.BatchNorm2d(num_filters * 4),
+            nn.ConvTranspose2d(num_filters * 4, num_filters * 2, 4, 2, 1, bias=False),
+            nn.ReLU(True),
+            nn.BatchNorm2d(num_filters * 2),
+            nn.ConvTranspose2d(num_filters * 2, num_filters, 4, 2, 1, bias=False),
+            nn.ReLU(True),
+            nn.BatchNorm2d(num_filters),
+            nn.ConvTranspose2d(num_filters, channels, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        return img
+
+
+class Critic(nn.Module):
+    def __init__(self, channels, num_filters):
+        super(Critic, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(channels, num_filters, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(num_filters),
+            nn.Conv2d(num_filters, num_filters * 2, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(num_filters * 2),
+            nn.Conv2d(num_filters * 2, num_filters * 4, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(num_filters * 4),
+            nn.Conv2d(num_filters * 4, num_filters * 8, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(num_filters * 8),
+            nn.Conv2d(num_filters * 8, 1, 4, 1, 0, bias=False)
+        )
+
+    def forward(self, img):
+        prob = self.model(img)
+        return prob.view(-1, 1).squeeze(1)
