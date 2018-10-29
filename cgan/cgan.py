34abe731ad8b24b275b583722c66c6dedd2f21af
TheRevanchist <ismail.elezi@gmail.com>
2018-10-29 21:53:56 +0100

    Clean conditional gan

--- cgan/cgan.py
+++ cgan/cgan.py
@@ -1,10 +1,10 @@
 # import needed pytorch modules
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.utils import save_image
 import torchvision.transforms as transforms
+from net import Generator, Discriminator
 
 # import other libraries
 import os
@@ -44,81 +44,6 @@ def weights_init(m):
         m.weight.data.normal_(0.0, 0.02)
 
 
-class Generator(nn.Module):
-    def __init__(self, batchnorm):
-        super(Generator, self).__init__()
-
-        if batchnorm:
-            self.model = nn.Sequential(
-                nn.Linear(opt.latent + opt.n_classes, 128),
-                nn.ReLU(),
-                nn.BatchNorm1d(128),
-                nn.Linear(128, 256),
-                nn.ReLU(),
-                nn.BatchNorm1d(256),
-                nn.Linear(256, 512),
-                nn.ReLU(),
-                nn.BatchNorm1d(512),
-                nn.Linear(512, 1024),
-                nn.ReLU(),
-                nn.BatchNorm1d(1024),
-                nn.Linear(1024, int(np.prod(img_shape))),
-                nn.Tanh()
-            )
-        else:
-            self.model = nn.Sequential(
-                nn.Linear(opt.latent + opt.n_classes, 128),
-                nn.ReLU(),
-                nn.Linear(128, 256),
-                nn.ReLU(),
-                nn.Linear(256, 512),
-                nn.ReLU(),
-                nn.Linear(512, 1024),
-                nn.ReLU(),
-                nn.Linear(1024, int(np.prod(img_shape))),
-                nn.Tanh()
-            )
-
-    def forward(self, z, labels):
-        image_and_label = torch.cat((z, labels), dim=1)
-        img = self.model(image_and_label)
-        img = img.view(img.size(0), *img_shape)
-        return img
-
-
-class Discriminator(nn.Module):
-    def __init__(self, batchnorm):
-        super(Discriminator, self).__init__()
-        if batchnorm:
-            self.model = nn.Sequential(
-                nn.Linear(int(np.prod(img_shape) + opt.n_classes), 512),
-                nn.ReLU(),
-                nn.Linear(512, 256),
-                nn.ReLU(),
-                nn.Linear(256, 128),
-                nn.ReLU(),
-                nn.Linear(128, 1),
-                nn.Sigmoid()
-            )
-        else:
-            self.model = nn.Sequential(
-                nn.Linear(int(np.prod(img_shape) + opt.n_classes), 512),
-                nn.ReLU(),
-                nn.Linear(512, 256),
-                nn.ReLU(),
-                nn.Linear(256, 128),
-                nn.ReLU(),
-                nn.Linear(128, 1),
-                nn.Sigmoid()
-            )
-
-    def forward(self, img, labels):
-        img_flat = img.view(img.size(0), -1)
-        image_and_label = torch.cat((img_flat, labels), dim=1)
-        prob = self.model(image_and_label)
-        return prob
-
-
 def one_hot_embedding(labels, num_classes):
     """Embedding labels to one-hot form.
 
@@ -134,8 +59,8 @@ def one_hot_embedding(labels, num_classes):
 
 # create the objects for loss function, two networks and for the two optimizers
 adversarial_loss = torch.nn.BCELoss()
-generator = Generator(batchnorm=True)
-discriminator = Discriminator(batchnorm=True)
+generator = Generator(batchnorm=True, latent=opt.latent, n_classes=opt.n_classes, img_shape=img_shape)
+discriminator = Discriminator(batchnorm=True, n_classes=opt.n_classes, img_shape=img_shape)
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.learning_rate, betas=(opt.beta_1, opt.beta_2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.learning_rate, betas=(opt.beta_1, opt.beta_2))
 
@@ -160,7 +85,7 @@ for epoch in range(opt.n_epochs):
         # train the generator
         optimizer_G.zero_grad()
         z = torch.FloatTensor(np.random.normal(0, 1, (inputs.shape[0], opt.latent))).to(device)
-        generated_images = generator(z, labels)
+        generated_images = generator(z, labels, img_shape)
 
         # measure the generator loss and do backpropagation
         g_loss = adversarial_loss(discriminator(generated_images, labels), real)
@@ -184,10 +109,10 @@ for epoch in range(opt.n_epochs):
                       5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9]
             labels = torch.LongTensor(labels)
             labels = one_hot_embedding(labels, opt.n_classes).to(device)
-            generated_images = generator(z, labels)
+            generated_images = generator(z, labels, img_shape)
             print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader),
                                                                              d_loss.item(), g_loss.item()))
-            save_image(generated_images.data, 'images/%d.png' % current_epoch, nrow=5, normalize=True)
+            save_image(generated_images.data, 'mnist/%d.png' % current_epoch, nrow=5, normalize=True)
             current_epoch += 1
 
 
